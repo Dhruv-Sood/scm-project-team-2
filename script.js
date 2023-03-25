2879326a66f525282e1c6d192bd26d03b20772b1
Dhruv Sood <dhruvsoodboss@gmail.com>
2023-03-25 20:56:02 +0530

    Initial Commit: Initialize  meme generator website

---
+++ script.js
@@ -0,0 +1,25 @@
+const memes = [
+    'https://i.redd.it/a0v87gwzoge61.jpg', 'https://i.redd.it/q29egav34ee61.jpg', 'https://i.redd.it/iij16swxjie61.jpg', 'https://i.redd.it/vek7dm2hrge61.jpg', 'https://www.testbytes.net/wp-content/uploads/2019/06/Untitled-8.png', 'https://miro.medium.com/max/1000/0*Ua695vjzFHV6VNOX.png', 'https://pbs.twimg.com/media/EKkPagPXkAA__Qo.jpg', 'https://code-love.com/wp-content/uploads/2019/03/download.jpeg', 'https://www.thecoderpedia.com/wp-content/uploads/2020/06/Programming-Memes-Programmer-while-sleeping.jpg', 'https://www.thecoderpedia.com/wp-content/uploads/2020/06/Programming-Memes-Evolution-of-Memory-Storage-1024x996.jpg', 'https://www.thecoderpedia.com/wp-content/uploads/2020/06/Programming-Memes-Error-in-Code-896x1024.jpg', 'https://www.thecoderpedia.com/wp-content/uploads/2020/06/Coding-Meme-Code-Comments-be-Like-925x1024.jpg', 'https://www.thecoderpedia.com/wp-content/uploads/2020/06/Internet-Explorer-Joke-915x1024.jpg'
+];
+
+const reset = ()=>{
+    const memeContainer = document.querySelector(".meme-img");
+    memeContainer.innerHTML = ""
+}
+
+const showImg = ()=>{
+    let meme = memes[Math.floor(Math.random()*(memes.length))]
+    const memeContainer = document.querySelector(".meme-img");
+
+    const imgContainer = document.createElement("img")
+    imgContainer.setAttribute("src" , meme)
+
+    memeContainer.appendChild(imgContainer)
+}
+
+console.log("Script working!!")
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', ()=>{
+    reset();
+    showImg();
+})
